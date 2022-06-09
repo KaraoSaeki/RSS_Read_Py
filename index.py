dc2c0d4e17ef8d3e420eb78aad8733566d6da92d
KaraoSaeki <liamvaganay61@gmail.com>
2022-06-09 10:28:15 +0200

    initialization + flux rss en lecture

---
+++ index.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import feedparser
+
+from function.feed_parser import feed_parser
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+        if message.content.startswith('https://www.cert.ssi.gouv.fr/alerte/feed/'):
+            await message.channel.send(feed_parser(message.content))
+
+client = MyClient()
+client.run('OTgzMzc2OTkwMDA0NDQ1MjQ0.G4lDaB.ABDR585VOIcnDxcxr82iyIB1qlO3MPxQ1RhDQA')
