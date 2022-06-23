016f798f07fe30f481944eb1b0bf4103bd403a6d
KaraoSaeki <liamvaganay61@gmail.com>
2022-06-23 13:54:07 +0200

    ajout d'embed a certaines commande

--- commande/list_commande.py
+++ commande/list_commande.py
@@ -1,20 +1,9 @@
-list_commande = {
+commande = {
     "help" :"affiche les commandes du bot",
     "rss" :"lecture d'un des flux dans la liste",
     "add" :"ajoute un flux",
     "remove" :"supprime un  flux que vous avez ajouté",
     "list" :"affiche la liste des flux",
     "ping" :"pong",
-    "" :"",
-    "" :"",
+    "quit" :"quitte le bot",
 }
-
-
-
-            #await message.channel.send("Voici la liste des commandes :")
-            #await message.channel.send("rss : affiche la liste des flux")
-            #await message.channel.send("add : ajoute un flux")
-            #await message.channel.send("remove : supprime un flux")
-            #await message.channel.send("list : affiche la liste des flux")
-            #await message.channel.send("help : affiche la liste des commandes")
-            #await message.channel.send("ping : pong")
--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+from itertools import count
+import numbers
 import discord
 from discord.ext import commands
 import feedparser
@@ -6,11 +8,9 @@ import sys
 default_intents = discord.Intents.default()
 default_intents.members = True
 
-sys.path.append('flux.py')
-sys.path.append('token.py')
-
 from token_id import token_id
 from flux import list_flux
+from commande.list_commande import commande
 from function.feed_parser import feed_parser
 
 class MyClient(discord.Client):
@@ -29,8 +29,12 @@ class MyClient(discord.Client):
 
         if message.content.startswith('rss'):
             await message.channel.send("Entrez le flux que vous voulez lire :")
+            embed = discord.Embed(color=0x00ff00)
+            number_flux = len(list_flux)
+            embed.add_field(name="Liste des flux", value=f"nombre de flux ={number_flux}", inline=False)
             for flux in list_flux:
-                await message.channel.send(f"{flux} : {list_flux[flux]}")
+                embed.add_field(name=flux, value=list_flux[flux], inline=False)
+            await message.channel.send(embed=embed)
 
             flux_choice = await self.wait_for('message', check=lambda message: message.author == message.author)
 
@@ -41,22 +45,23 @@ class MyClient(discord.Client):
                 title, link = feed_parser(list_flux[choix])
                 await message.channel.send("{} \n{}".format(title, link))
             else:
-                await message.channel.send("Vous avez choisi le flux n°{}".format(choix))
-                await message.channel.send(list_flux[choix])
+                await message.channel.send("Votre choix n'est pas dans la liste")
         
         if message.content.startswith('add'):
-            await message.channel.send("Entrez le lien que vous voulez ajouter :")
-            flux_add = await self.wait_for('message', check=lambda message: message.author == message.author)
-            await message.channel.send("Vous avez ajouté le flux : {}".format(flux_add.content))
-            list_flux[len(list_flux)+1] = flux_add.content
-            await message.channel.send("Veuillez patienter...")
-            title, link = feed_parser(flux_add.content)
-            await message.channel.send("{} \n{}".format(title, link))
+            args = message.content.split(" ")
+            args.pop(0)
+            for arg in args:
+                await message.channel.send(f"Vous avez ajouté le flux : {arg}")
+                list_flux[len(list_flux)+1] = arg
+                await message.channel.send("Votre flux a été ajouté avec succès")
 
         if message.content.startswith('list'):
-            await message.channel.send("Voici la liste des flux :")
+            embed = discord.Embed(color=0x00ff00)
+            number_flux = len(list_flux)
+            embed.add_field(name="Liste des flux", value=f"nombre de flux ={number_flux}", inline=False)
             for flux in list_flux:
-                await message.channel.send(f"{flux} : {list_flux[flux]}")
+                embed.add_field(name=flux, value=list_flux[flux], inline=False)
+            await message.channel.send(embed=embed)
         
         if message.content.startswith('remove'):
             await message.channel.send("Entrez le flux que vous voulez supprimer :")
@@ -64,16 +69,13 @@ class MyClient(discord.Client):
             flux_remove = int(flux_remove.content)
             await message.channel.send("Vous avez supprimé le flux : {}".format(flux_remove))
             del list_flux[flux_remove]
-            await message.channel.send("Veuillez patienter...")
-            title, link = feed_parser(flux_remove)
-            await message.channel.send("{} \n{}".format(title, link))
+            await message.channel.send("Votre flux a été supprimé avec succès")
 
         if message.content.startswith('help'):
             embed = discord.Embed(color=0x00ff00)
-            embed.add_field(name="rss", value="affiche la liste des flux", inline=False)
-            embed.add_field(name="rss", value="affiche la liste des flux", inline=False)
-
-            await message.channel.send(message.channel, embed=embed)
+            for command in commande:
+                embed.add_field(name=command, value=commande[command], inline=False)
+            await message.channel.send(embed=embed)
         
         if message.content.startswith('exit'):
             await message.channel.send("Au revoir !")
